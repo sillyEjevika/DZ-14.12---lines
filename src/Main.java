c1f19c0d47d848a0275d608981addd477342e3e0
sillyEjevika <Doroteia_Vika@mail.ru>
2022-12-15 15:27:43 +0300

    Задача №1-3 (строки)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        // ЗАдача №1
+        System.out.println("Задача №1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        System.out.println( "ФИО сотрудника - " + lastName + " " + firstName + " "+ middleName);
+        // ЗАдача №2
+        System.out.println("Задача №2");
+        String smallFullName = "Ivanov Ivan Ivanovich";
+        System.out.println(smallFullName.toUpperCase());
+        // Задача №3
+        System.out.println("Задача №3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
     }
 }
